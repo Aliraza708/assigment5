c28ac5ea5c1b54f441d099c85cc72c4342813ded
Aliraza <155419928+Aliraza708@users.noreply.github.com>
2024-09-26 18:22:03 +0500

    image update

--- src/ContextApi/ProductContext.jsx
+++ src/ContextApi/ProductContext.jsx
@@ -7,7 +7,7 @@ import image4 from '../assets/images/Images9.png';
 import image5 from '../assets/images/Images3.png';
 import image6 from '../assets/images/Images2.png';
 import image7 from '../assets/images/Images5.png';
-// import image8 from '../assets/images/Images8.png';
+import image8 from '../assets/images/images4.png';
 
 
 
@@ -22,7 +22,7 @@ function ProductContextProvider({ children }) {
     { title: 'Grifo', id: 5, categories: 'Night lamp', price: "1.500.000", image: image5 },
     { title: 'Muggo', id: 6, categories: 'Small mug', price: "150.000", image: image6 },
     { title: 'Pingky', id: 7, categories: 'Cute bed set', price: "7.00.000", image: image7 },
-    // { title: 'Potty', id: 8, categories: 'Minimalist flower pot', price: "500.000", image: image8 }
+    { title: 'Potty', id: 8, categories: 'Minimalist flower pot', price: "500.000", image: image8 }
     ,
    
   ]
